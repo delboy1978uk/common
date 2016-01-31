d0ec2301317f40739225685109d0dff47867c5a1
Derek Stephen McLean <delboy1978uk@gmail.com>
2016-01-31 08:05:32 +0000

    added json method to controller abstract

--- src/ZF1/Controller/AbstractController.php
+++ src/ZF1/Controller/AbstractController.php
@@ -3,17 +3,13 @@
 namespace Del\Common\ZF1\Controller;
 
 use Del\Common\ContainerService;
+use Exception;
+use Zend_Auth;
 use Zend_Controller_Action;
+use Zend_Layout;
 
 class AbstractController extends Zend_Controller_Action
 {
-    /** @var  \Pimple\Container $container */
-    private $container;
-
-    public function init()
-    {
-        $this->container = ContainerService::getInstance()->getContainer();
-    }
 
     /**
      * @param $key
@@ -21,6 +17,25 @@ class AbstractController extends Zend_Controller_Action
      */
     public function getContainerObject($key)
     {
-        return $this->container[$key];
+        return ContainerService::getInstance()->getContainer()[$key];
+    }
+
+    /**
+     *
+     */
+    public function sendJSONResponse(array $array)
+    {
+        Zend_Layout::getMvcInstance()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $this->getResponse()->setHeader('Content-Type', 'application/json');
+        $this->_helper->json($array);
+    }
+
+    public function getIdentity()
+    {
+        if(Zend_Auth::getInstance()->hasIdentity()) {
+            return Zend_Auth::getInstance()->getIdentity();
+        }
+        throw new Exception('No Identity.');
     }
 }
